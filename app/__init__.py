ff61223c2b174456c9b6ade4299db05ddea6792e
Gaston <gonzalezgastonariel@gmail.com>
2023-08-20 11:16:06 -0300

    Commit desde el punto 9 hasta el 14

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
 from config import Config
+from datetime import datetime
+import json
 
-from .utils.utilstp3 import calcular_edad, format_title_case
+from .utils.utilstp3 import calcular_edad, format_title_case, quitar_signos, format_name, validate_dni, convert_binary_to_decimal, is_balanced
 
 def init_app():
     """Crea y configura la aplicación Flask"""
@@ -138,11 +140,128 @@ def init_app():
             "titulo": format_title_case(word)
         }
         return (response, 200, {'Content-Type': 'application/json'})
-
-
-
-             
     
+    
+    
+    #EJ9
+    @app.route('/formatted/<string:dni>')
+    def convertir_dni(dni):
+        dni_convertido = quitar_signos(dni)
+        resultado = {"formatted_dni":dni_convertido}
+        return jsonify(resultado)
+    
+    
+    
+    #EJ10
+    @app.route('/format')
+    def format_user_data():
+        
+        firstname = request.args.get('firstname')
+        lastname = request.args.get('lastname')
+        dob = request.args.get('dob')
+        dni = request.args.get('dni')        
+        
+        formatted_firstname = format_name(firstname)
+        formatted_lastname = format_name(lastname)        
+        
+        dob_date = datetime.strptime(dob, '%Y-%m-%d')
+        today = datetime.today()
+        age = today.year - dob_date.year - ((today.month, today.day) < (dob_date.month, dob_date.day))        
+        
+        dni_numeric = validate_dni(dni)
+        if dni_numeric is None:
+            error_message = {'error': 'El DNI no es válido'}
+            return jsonify(error_message), 400        
+        
+        response = {
+            'firstname': formatted_firstname,
+            'lastname': formatted_lastname,
+            'age': age,
+            'dni': dni_numeric
+        }
+        
+        return jsonify(response)
+    
+
+
+#EJ11
+
+    with open('morse_code.json') as file:
+     morse_code_dict = json.load(file)['letters']
+   
+    def encode_to_morse(keyword):
+        morse_encoded = []
+        for char in keyword:
+            if char == '+':
+                morse_encoded.append('^')
+            else:
+                morse_encoded.append(morse_code_dict.get(char, char))
+        return '+'.join(morse_encoded)
+    
+    @app.route('/encode/<string:keyword>')
+    def encode_keyword(keyword):        
+        keyword = keyword.replace('^', ' ')        
+        if not keyword.isalnum() or len(keyword) > 100:
+            error_message = {'error': 'Palabra clave no válida'}
+            return jsonify(error_message), 400
+        
+        morse_encoded = encode_to_morse(keyword.upper())
+        response = {'morse_encoded': morse_encoded}
+        
+        return jsonify(response)
+    
+
+
+    #EJ12
+    with open('morse_code.json') as file:
+     morse_code_dict = json.load(file)['letters']
+
+    def decode_from_morse(morse_code):
+        morse_parts = morse_code.split('+')
+        decoded = []
+        for part in morse_parts:
+            char = next((key for key, value in morse_code_dict.items() if value == part), None)
+            if char is not None:
+                decoded.append(char)
+            elif part == '^':
+                decoded.append(' ')
+        return ''.join(decoded)
+
+    @app.route('/decode/<string:morse_code>')
+    def decode_morse_code(morse_code):
+        plaintext = decode_from_morse(morse_code)
+        response = {'plaintext': plaintext}
+        
+        return jsonify(response)
+    
+
+
+    #EJ13
+    @app.route('/convert/binary/<string:num>')
+    def convert_binary(num):
+        if not all(bit in ['0', '1'] for bit in num):
+            error_message = {'error': 'Número binario inválido'}
+            return jsonify(error_message), 400
+        
+        decimal_result = convert_binary_to_decimal(num)
+        response = {'decimal': decimal_result}
+        
+        return jsonify(response)
+    
+
+
+    #EJ14
+    @app.route('/balance/<string:input>')
+    def check_balance(input):
+        if is_balanced(input):
+            response = {'balanced': True}
+        else:
+            response = {'balanced': False}
+        
+        return jsonify(response)
+
+
+
     
     @app.route('/academia/<string:user>')
     def academia(user):
--- app/utils/utilstp3.py
+++ app/utils/utilstp3.py
@@ -17,3 +17,54 @@ def format_title_case(input_string):
     return formatted_string
 
 
+#EJ9
+def quitar_signos(dni1):
+    dni="".join(numero for numero in dni1 if numero.isdigit())
+    if len(dni) != 8 or dni[0]=='0':
+        return 'Error, no valido'
+    return int(dni)
+
+
+#EJ10
+def format_name(name):
+    return ' '.join([part.capitalize() for part in name.split()])
+
+def validate_dni(dni):
+    dni_numeric = ''.join(char for char in dni if char.isdigit())
+    
+    if len(dni_numeric) != 8:
+        return None
+    
+    return int(dni_numeric)
+
+
+#EJ13
+def convert_binary_to_decimal(binary):
+    decimal = 0
+    length = len(binary)
+    
+    for i in range(length):
+        if binary[i] == '1':
+            exponent = length - 1 - i
+            decimal += 2 ** exponent
+            
+    return decimal
+
+
+#EJ14
+def is_balanced(input_string):
+    stack = []
+    opening_symbols = '([{'
+    closing_symbols = ')]}'
+    
+    for char in input_string:
+        if char in opening_symbols:
+            stack.append(char)
+        elif char in closing_symbols:
+            if not stack:
+                return False
+            top_symbol = stack.pop()
+            if opening_symbols.index(top_symbol) != closing_symbols.index(char):
+                return False
+    
+    return len(stack) == 0    
